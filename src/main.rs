d6ddfac9b162dee96c3acbb5903a8a0b4136a6f0
Cristian Prieto <me@cprieto.com>
2024-02-25 17:29:59 +0100

    Add syscall for x86_64 freebsd

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use core::arch::asm;
 
 #[cfg(all(target_arch = "x86_64", target_os = "linux"))]
 #[inline(never)]
-fn linux_raw_syscall(message: String) {
+fn raw_syscall(message: String) {
     let message_ptr = message.as_ptr();
     let len = message.len();
 
@@ -21,7 +21,29 @@ fn linux_raw_syscall(message: String) {
     }
 }
 
+
+#[cfg(all(target_arch = "x86_64", target_os = "freebsd"))]
+#[inline(never)]
+fn raw_syscall(message: String) {
+    let message_ptr = message.as_ptr();
+    let len = message.len();
+
+    unsafe {
+        asm!(
+            "mov rax, 4",
+            "mov rdi, 1",
+            "syscall",
+            in("rsi") message_ptr,
+            in("rdx") len,
+            out("rax") _,
+            out("rdi") _,
+            lateout("rsi") _,
+            lateout("rdx") _,
+        );
+    }
+}
+
 fn main() {
     let message = String::from("Hello world from raw syscall\n");
-    linux_raw_syscall(message);
+    raw_syscall(message);
 }
